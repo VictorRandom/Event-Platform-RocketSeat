54e90b1fb74d50135ea90b4e548f12fac2eb187a
VictorRandom <victormbalduino@hotmail.com>
2022-08-01 16:14:00 -0300

    first class

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+import Lesson from "./components/Lesson"
+import Siderbar from "./components/Sidebar"
+import Video from "./components/Video"
+import Event from "./pages/Event"
+
+
+function App() {
+
+  return (
+    <div>
+      <Event />
+    </div>
+    )
+}
+
+export default App
